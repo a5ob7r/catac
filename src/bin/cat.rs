14ea273e4eefcbe54a95fe35b936ef8e5398d63b
a5ob7r <12132068+a5ob7r@users.noreply.github.com>
2021-07-18 21:05:18 +0900

    Use buffer to read file instead of reading all contents at once

--- src/bin/cat.rs
+++ src/bin/cat.rs
@@ -1,13 +1,28 @@
 use std::io;
+use std::process;
+use std::str;
 use std::{env, fs::File, io::Read};
 
+const BUF_SIZE: usize = 1024 * 4;
+
+// TODO: Really valid size? How treats the case if intermediate of valid UTF-8 sequence is on
+// `BUF_SIZE` times index position on file?.
 fn cat(path: &String) -> io::Result<()> {
     let mut file = File::open(path)?;
 
-    let mut contents = String::new();
-    file.read_to_string(&mut contents)?;
+    let mut buf = [0; BUF_SIZE];
+    while let Ok(n) = file.read(&mut buf) {
+        if n == 0 {
+            break;
+        };
 
-    println!("{}", contents);
+        match str::from_utf8(&buf[..n]) {
+            Ok(s) => print!("{}", s),
+            Err(err) => {
+                return Err(io::Error::new(io::ErrorKind::Other, err));
+            }
+        }
+    }
 
     return Ok(());
 }
@@ -16,6 +31,7 @@ fn main() {
     for arg in env::args().skip(1) {
         if let Err(err) = cat(&arg) {
             eprintln!("{}: {}", arg, err);
+            process::exit(1);
         }
     }
 }
