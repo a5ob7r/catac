f2c7b6bdae38acd1364b0cf36909a024acb2c3a1
a5ob7r <12132068+a5ob7r@users.noreply.github.com>
2021-07-18 19:55:35 +0900

    Remove redundant error handling

--- src/bin/cat.rs
+++ src/bin/cat.rs
@@ -1,28 +1,21 @@
+use std::io;
 use std::{env, fs::File, io::Read};
 
-fn cat(path: &String) {
-    let mut file = match File::open(path) {
-        Ok(file) => file,
-        Err(err) => {
-            eprintln!("{}: {}", path, err);
-            return;
-        }
-    };
+fn cat(path: &String) -> io::Result<()> {
+    let mut file = File::open(path)?;
 
     let mut contents = String::new();
-    match file.read_to_string(&mut contents) {
-        Err(err) => {
-            eprintln!("{}: {}", path, err);
-            return;
-        }
-        _ => {}
-    };
+    file.read_to_string(&mut contents)?;
 
     println!("{}", contents);
+
+    return Ok(());
 }
 
 fn main() {
     for arg in env::args().skip(1) {
-        cat(&arg);
+        if let Err(err) = cat(&arg) {
+            eprintln!("{}: {}", arg, err);
+        }
     }
 }
--- src/bin/tac.rs
+++ src/bin/tac.rs
@@ -1,32 +1,25 @@
 use std::env;
 use std::fs::File;
+use std::io;
 use std::io::Read;
 
-fn tac(path: &String) {
-    let mut file = match File::open(path) {
-        Ok(file) => file,
-        Err(err) => {
-            eprintln!("{}: {}", path, err);
-            return;
-        }
-    };
+fn tac(path: &String) -> io::Result<()> {
+    let mut file = File::open(path)?;
 
     let mut contents = String::new();
-    match file.read_to_string(&mut contents) {
-        Err(err) => {
-            eprintln!("{}: {}", path, err);
-            return;
-        }
-        _ => {}
-    };
+    file.read_to_string(&mut contents)?;
 
     for s in contents.rsplit("\n") {
         println!("{}", s);
     }
+
+    return Ok(());
 }
 
 fn main() {
     for arg in env::args().skip(1) {
-        tac(&arg);
+        if let Err(err) = tac(&arg) {
+            eprintln!("{}: {}", arg, err);
+        }
     }
 }
